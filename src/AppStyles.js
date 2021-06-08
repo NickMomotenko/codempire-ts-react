0fa6133e805c79545ee58655c48f579864545914
nick momotenko <nickmomotenkoforweb@gmail.com>
2021-06-08 15:24:10 +0300

    add global styles , fixed header data ,delete index.css

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,6 +1,42 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap");
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body,
+    html {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        font-family: "Raleway", sans-serif;
+    }
+
+    #root {
+        height: 100%;
+        width: 100%;
+    }
+
+    ul,
+    li {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    button {
+        outline: none;
+        border: 1px solid transparent;
+        padding: 0;
+    }
+
+`;
 
 export const AppWrapp = styled.div`
-    height:100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useData } from "../../hooks/data";
 
 import Container from "../../UI/Container";
 import Logo from "../../UI/Logo";
@@ -7,8 +6,6 @@ import Logo from "../../UI/Logo";
 import { HeaderWrapp } from "./styles";
 
 const Header = () => {
-  const { data } = useData();
-
   return (
     <HeaderWrapp>
       <Container>
